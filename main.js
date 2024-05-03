af91c88138a2e3f9348a15b2e588d25b5d93ae93
JHX549 <jiang_549@outlook.com>
2024-05-02 17:47:17 -0700

    Update main.js

--- main.js
+++ main.js
@@ -263,9 +263,60 @@ function applyImageDataFilter(imageData) {
 }
 
 
-// Menu fade in
+// // Check if the current page is index.html
+// if (window.location.pathname === '/index.html') {
+//     // Menu fade in
+//     window.addEventListener('scroll', function() {
+//         var menubox = document.getEle// Menu fade in
+//         window.addEventListener('scroll', function() {
+//           var menubox = document.getElementById('menubox');
+//           if (window.scrollY > 100) {
+//               if (menubox.style.display !== 'block') {
+//                   menubox.style.display = 'block';
+//                   fadeIn(menubox); // Call fadeIn function to add fade-in effect
+//               }
+//           } else {
+//               menubox.style.display = 'none';
+//           }
+//         });
+        
+//         function fadeIn(element) {
+//           var opacity = 0;
+//           var intervalID = setInterval(function() {
+//               if (opacity < 1) {
+//                   opacity += 0.1;
+//                   element.style.opacity = opacity;
+//               } else {
+//                   clearInterval(intervalID);
+//               }
+//           }, 50);
+//         }mentById('menubox');
+//         if (window.scrollY > 100) {
+//             if (menubox.style.display !== 'block') {
+//                 menubox.style.display = 'block';
+//                 fadeIn(menubox); // Call fadeIn function to add fade-in effect
+//             }
+//         } else {
+//             menubox.style.display = 'none';
+//         }
+//     });
+// }
+
+// function fadeIn(element) {
+//     var opacity = 0;
+//     var intervalID = setInterval(function() {
+//         if (opacity < 1) {
+//             opacity += 0.1;
+//             element.style.opacity = opacity;
+//         } else {
+//             clearInterval(intervalID);
+//         }
+//     }, 50);
+// }
+
 // Check if the current page is index.html
 if (window.location.pathname === '/index.html') {
+    // Menu fade in
     window.addEventListener('scroll', function() {
         var menubox = document.getElementById('menubox');
         if (window.scrollY > 100) {
@@ -290,4 +341,3 @@ function fadeIn(element) {
         }
     }, 50);
 }
-
